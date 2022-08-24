f522845acefb0ea53f69a3d0c9e8c9e81dc7ac13
Sussanara <sussanaracely@gmail.com>
2022-08-24 03:31:25 +0000

    Datos ok

--- src/app.py
+++ src/app.py
@@ -60,15 +60,35 @@ def handle_hello():
         }
         jackson_family.add_member(member)
         return jsonify({"msg":"Member created successfully"}), 200
-        
-                
 
+    if request.method =="PUT":
+        member = jackson_family.get_member(id)    
+        first_name = request.json.get("first_name")
+        last_name = "Jackson"
+        age = request.json.get("age")
+        lucky_numbers = request.json.get("lucky_numbers")
 
+        if not first_name:
+               return jsonify({"msg": "first_name is requires"}), 400
+        if not age:
+            return jsonify({"msg": "age is requires"}), 400
+        if not lucky_numbers:
+            return jsonify({"msg": "lucky_numbers is required"}), 400
 
+        member["first_name"] = first_name
+        member["age"] = age
+        member["lucky_numbers"] = lucky_numbers
 
+        return jsonify({"msg": "Member created successfully"}, member), 200
 
+    if request.method =="DELETE":
+        member = jackson_family.get_member(id)
+        if not member:
+            return jsonify({"msg": "Member not found"}), 404
+        else:
+            jackson_family.delete_member(id)
+            return jsonify({"done": True}), 200
 
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
